ab762912241834ed793a4f8daa45d81ee44545dd
Clemens S <clemens.strobel@web.de>
2024-01-04 16:17:18 +0100

    Progress

--- game/Assets/Scripts/InputManagement/MouseClickListener.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface MouseClickListener {
-    void groundPlaneClicked(Vector3 positionInWorld);
-    void gameObjectClicked(GameObject gameObject, Vector3 positionInWorld);
-}
